85d6efda2bedb7824801fa2b8c6cfbbc0dbcb215
Ahmad Mostafa <ahmed.elasiri1@gmail.com>
2025-11-27 22:34:59 +0200

    feat: add middleware chain and logging

---
+++ server/internal/middleware/json.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"tyk-registration-server/internal/models"
+	"tyk-registration-server/internal/response"
+)
+
+const registrationReqKey = "registration_req"
+
+func ParseRegistrationJSON() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var req models.RegistrationRequest
+		if err := c.BodyParser(&req); err != nil {
+			return response.SendError(c, fiber.StatusBadRequest, response.NewValidationError("Invalid JSON payload", nil))
+		}
+		c.Locals(registrationReqKey, &req)
+		return c.Next()
+	}
+}
+
+func GetRegistrationFromCtx(c *fiber.Ctx) *models.RegistrationRequest {
+	val := c.Locals(registrationReqKey)
+	if val == nil {
+		return nil
+	}
+	req, _ := val.(*models.RegistrationRequest)
+	return req
+}
+
+
+
+
---
+++ server/internal/middleware/logging.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Logging() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+		err := c.Next()
+		latency := time.Since(start)
+
+		status := c.Response().StatusCode()
+		log.Printf("%s %s %d %s", c.Method(), c.Path(), status, latency)
+
+		return err
+	}
+}
+
+
+
+
