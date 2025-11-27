33114b4326e4a8e9c75c0ee7c6fd2546e0b789b5
Ahmad Mostafa <ahmed.elasiri1@gmail.com>
2025-11-27 22:34:57 +0200

    feat: add user service with registration logic

---
+++ server/internal/services/user_service.go
@@ -0,0 +1,35 @@
+package services
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"tyk-registration-server/internal/models"
+	"tyk-registration-server/internal/repositories"
+	"tyk-registration-server/internal/utils"
+)
+
+type UserService interface {
+	Register(ctx context.Context, req *models.RegistrationRequest) (uuid.UUID, error)
+}
+
+type userService struct {
+	repo repositories.UserRepository
+}
+
+func NewUserService(repo repositories.UserRepository) UserService {
+	return &userService{repo: repo}
+}
+
+func (s *userService) Register(ctx context.Context, req *models.RegistrationRequest) (uuid.UUID, error) {
+	hash, err := utils.HashPassword(req.Password)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return s.repo.CreateUser(ctx, req, hash)
+}
+
+
+
+
